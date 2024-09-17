deebf44d5ad8f66c8880779a1a6fcd2f9f0270ae
ZTB97 <n11029153@outlook.com>
2024-09-17 21:17:03 +1000

    Refactored 'list' class to Category

--- src/main/java/com/example/cab302simplestock/model/Category.java
+++ src/main/java/com/example/cab302simplestock/model/Category.java
@@ -1,8 +1,35 @@
 package com.example.cab302simplestock.model;
 
+import java.util.ArrayList;
+
 public class Category {
-    /*
-    name
-    items
-     */
+    private int id;
+    private String name;
+    private java.util.List<Item> items;
+
+    // Constructor
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.items = new ArrayList<>();
+    }
+
+    // Functions to add and remove items
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void removeItem(Item item) {
+        items.remove(item);
+    }
+
+    // Das getters und das setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public java.util.List<Item> getItems() { return items; }
+
 }
