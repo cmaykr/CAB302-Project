0f994d61d22f916077bf1493efd12ec9de862d85
cmaykr <dj@netatonce.net>
2024-09-18 12:30:48 +1000

    Minor changes

--- src/main/java/com/example/cab302simplestock/controller/RegistrationPage.java
+++ src/main/java/com/example/cab302simplestock/controller/RegistrationPage.java
@@ -1,15 +1,21 @@
 package com.example.cab302simplestock.controller;
 
+import com.example.cab302simplestock.HelloApplication;
 import com.example.cab302simplestock.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
 import com.example.cab302simplestock.model.SqliteUserDAO;
+import javafx.stage.Stage;
 
 public class RegistrationPage {
     @FXML
@@ -22,8 +28,10 @@ public class RegistrationPage {
     PasswordField password;
     @FXML
     PasswordField confirmPassword;
+    @FXML
+    Button SignUpButton;
 
-    public void onCreateAccount(ActionEvent actionEvent) {
+    public void onCreateAccount(ActionEvent actionEvent) throws IOException {
         SqliteUserDAO userDAO = new SqliteUserDAO();
         if (!Objects.equals(password.getText(), confirmPassword.getText()))
         {
@@ -33,6 +41,11 @@ public class RegistrationPage {
         userDAO.addUser(newUser);
 
         List<User> users = userDAO.getAllUsers();
-        System.out.println(users.get(0).getFirstName());
+        System.out.println(users.get(users.size()-1).getFirstName());
+
+        Stage stage = (Stage) SignUpButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
     }
 }
