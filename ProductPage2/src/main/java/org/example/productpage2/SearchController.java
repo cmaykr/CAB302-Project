b9b182badd0c4fade2be3b00e5b65be17466f677
Hossein273 <hssnalirahimi2000@gmail.com>
2024-09-20 18:26:27 +1000

    Add files via upload
    Product page almost complete

--- ProductPage2/src/main/java/org/example/productpage2/SearchController.java
+++ ProductPage2/src/main/java/org/example/productpage2/SearchController.java
@@ -1,5 +1,46 @@
 package org.example.productpage2;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 public class SearchController {
+    @FXML
+    private Button addItemsButton;
+    @FXML
+    private Button backButton;
+    @FXML
+    private Button viewItemsButton;
+    @FXML
+    protected void addItemsButton() throws IOException{
+        Stage stage = (Stage)addItemsButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),HelloApplication.WIDTH,HelloApplication.HEIGHT);
+        stage.setScene(scene);
+    }
+    @FXML
+    protected void backButtonClick() throws IOException{
+        Stage stage = (Stage)backButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("search-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),HelloApplication.WIDTH,HelloApplication.HEIGHT);
+        stage.setScene(scene);
+    }
+    @FXML
+    protected void viewItem() throws IOException{
+        Stage stage = (Stage)viewItemsButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("list-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),HelloApplication.WIDTH,HelloApplication.HEIGHT);
+        stage.setScene(scene);
+    }
+
+
+
 
 }
