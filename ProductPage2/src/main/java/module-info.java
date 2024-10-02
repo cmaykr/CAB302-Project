52396812c0f034ddaa03fcc011a39a91b935be82
Ali <Ali @gmail.com>
2024-10-02 13:25:04 +1000

    product page2 deleted refactored

--- ProductPage2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.productpage2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens org.example.productpage2 to javafx.fxml;
-    exports org.example.productpage2;
-}
