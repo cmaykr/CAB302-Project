57c9ba7f552e25d6c0053704ea9faf45ed17ce31
cmaykr <dj@netatonce.net>
2024-10-15 18:51:10 +1000

    Adds javadoc documentation to UserManager

--- src/main/java/com/example/cab302simplestock/model/UserManager.java
+++ src/main/java/com/example/cab302simplestock/model/UserManager.java
@@ -1,11 +1,18 @@
 package com.example.cab302simplestock.model;
 
+/**
+ * Singleton class used to keep track of the current logged-in user.
+ */
 public class UserManager {
     private static UserManager instance;
     private User loggedInUser = null;
 
     private UserManager() { }
 
+    /**
+     * Gets the singleton instance of the UserManager, if the instance doesn't exist a new instance is created.
+     * @return The instance of UserManager.
+     */
     public static UserManager getInstance() {
         if (instance == null)
             instance = new UserManager();
@@ -13,11 +20,20 @@ public class UserManager {
         return instance;
     }
 
+    /**
+     * Gets the current logged-in user for the application.
+     * @return The logged-in user as User type.
+     */
     public User getLoggedInUser() {
         return loggedInUser;
     }
 
-    public void setLoggedInUser(User newUser) {
+    /**
+     * Sets a new logged-in user as a User type and stores all user information.
+     * @param newUser The new user to be logged-in.
+     * @throws IllegalStateException Throws if the last logged-in user has not logged-out.
+     */
+    public void setLoggedInUser(User newUser) throws IllegalStateException {
         if (loggedInUser != null)
         {
             throw new IllegalStateException("A user is already logged in!");
@@ -25,6 +41,9 @@ public class UserManager {
         loggedInUser = newUser;
     }
 
+    /**
+     * Logs out the logged-in user.
+     */
     public void logOutUser() {
         loggedInUser = null;
     }
